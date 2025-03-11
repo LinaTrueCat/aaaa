c3359392f25e8cfc6b756aec2f8460a3d960eb7f
LinaTrueCat <146427251+LinaTrueCat@users.noreply.github.com>
2025-03-11 21:38:25 +0200

    add main.cpp

---
+++ Book/book/main.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include "recepy.h"
+#include "nps.h"
+
+using namespace std;
+
+int main() {
+    cout << "Opening book..." << endl;
+    {
+        Recepy grecha("Greecha", 50, true);
+        grecha.showStatus();
+        grecha.meatStatus();
+
+        Recepy potato("Potato", 20, false);
+        potato.showStatus();
+        potato.meatStatus();
+
+        Nps bob("Bob", 1, true);
+        bob.showStatus();
+        bob.showVegStatus();
+        bob.showWish();
+    }
+
+    cout << "Close book" << endl;
+    return 0;
+}
